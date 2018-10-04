ed019f1b15ed5b8495891499156082e3c8fb983c
Perez-Silva, Davian S <dsperezsilva171@stevenscollege.edu>
2018-10-04 10:18:34 -0400

    Fixed error now its working

--- lebanon.py
+++ lebanon.py
@@ -8,11 +8,13 @@ values = []
 
 with open("lebanon.csv") as csvfile:
     reader = csv.reader(csvfile)
-    #next(reader, None)
+    next(reader, None)
     for row in reader:
 
         dates.append(row[1])
-        values.append(row[2])
+        values.append(int(row[2]))
 plt.bar(dates, values)
+plt.xlabel("Age Range")
+plt.ylabel("Population")
 plt.xticks(rotation=45)
 plt.show()
